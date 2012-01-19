bbd2dbf02ea1fc91afd23197afeef7cc20c5f5a2
1 <1@comp6.(none)>
2012-01-19 21:17:56 +0300

    [version=1.15|3]

--- git_.bat
+++
@@ -1,2 +0,0 @@
-path F:\cygwin\usr\local\bin;F:\cygwin\bin;F:\cygwin\bin;%PATH%
-git %1 %2 %3 %4 %5 %6 %7 %8 %9
--- git_push.bat
+++
@@ -1,3 +0,0 @@
-path F:\cygwin\usr\local\bin;F:\cygwin\bin;F:\cygwin\bin;%PATH%
-git push -u origin master
-rem
